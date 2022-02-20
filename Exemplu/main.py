dd67f16881379f350734b6d3205bade7d52f0b21
BazaochiDumitru <bazaochi.dumitru@mail.ru>
2022-02-20 13:08:10 +0200

    Created main.py file

---
+++ Exemplu/main.py
@@ -0,0 +1,11 @@
+# print("Bazaochi Dumitru")
+
+# sir1 = "Acesta este un sir de caractere in python"
+# sir2 = "Salut, grupa IS31Z"
+# print(sir1)
+#
+# print ('Ana' + ' are'+ ' 7' + ' mere')
+# print('Ana' + ' are' + ' 7' + ' mere')
+# print('Ana' + ' are' + ' 2' + ' mere')
+# print('Ana ' * 5)
+# print(f'Ana are {5} mere')
