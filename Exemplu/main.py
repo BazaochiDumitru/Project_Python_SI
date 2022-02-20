c06f1da4b5a5211e351edbcd2c680da932ed42f3
BazaochiDumitru <bazaochi.dumitru@mail.ru>
2022-02-20 13:10:07 +0200

    Import Decimal library

--- Exemplu/main.py
+++ Exemplu/main.py
@@ -9,3 +9,25 @@
 # print('Ana' + ' are' + ' 2' + ' mere')
 # print('Ana ' * 5)
 # print(f'Ana are {5} mere')
+
+from decimal import Decimal
+
+# print (Decimal('10.10')+10)
+# print (Decimal('10.10')+10.01)
+# print (type (10))
+# print (type ('10'))
+# print (type (10.0))
+
+'''print(a[0:2]) 
+print(a[1:-1]) 
+print(a[0:]) 
+print(a[:2]) 
+print(a[:1]) 
+a[0] = 3 
+print(a[0:2])'''
+
+'''del a[4] 
+print(a) 
+a = (1,2, 3,4,5] 
+a.remove(4) 
+print(a)'''
